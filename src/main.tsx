6fa8d5e9e38c3d08314021f122892b5f4e7b33dc
kaksv <kakoozavian@gmail.com>
2025-11-01 22:01:05 +0300

    added metamask

--- src/main.tsx
+++ src/main.tsx
@@ -30,6 +30,7 @@ createAppKit({
 
   adapters: [wagmiAdapter, solanaWeb3JsAdapter],
     featuredWalletIds: [
+      "c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96",
     "1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369",
     "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0",
     "107bb20463699c4e614d3a2fb7b961e66f48774cb8f6d6c1aee789853280972c",
