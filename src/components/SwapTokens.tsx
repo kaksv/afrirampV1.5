cdca1af2723f34e747459afa39b2d7181c7f550c
kaksv <kakoozavian@gmail.com>
2025-12-23 12:17:07 +0300

    added fees

--- src/components/SwapTokens.tsx
+++ src/components/SwapTokens.tsx
@@ -2,6 +2,7 @@ import { LiFiWidget, WidgetConfig } from '@lifi/widget';
 
 const widgetConfig: WidgetConfig = {
   integrator: 'afriRamp',
+  fee: 0.002,
   theme: {
     container: {
       border: '1px solid rgb(234, 234, 234)',
